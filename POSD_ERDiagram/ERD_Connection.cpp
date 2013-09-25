1ad1a69be93bfc3ec55ab00530e3a2c17ccc279b
candybaby026 <candybaby026@gmail.com>
2013-09-25 20:48:48 +0800

    fix connect text show cardinality

--- POSD_ERDiagram/ERD_Connection.cpp
+++ POSD_ERDiagram/ERD_Connection.cpp
@@ -16,11 +16,13 @@ ERD_Connection::ERD_Connection(int id, ConnectionCardinality cardinality)
 	this->cardinality = cardinality;
 	this->type = Connection;
 	this->id = id;
+	this->text = ConnectionCardinalityNames[cardinality];
 }
 
 void ERD_Connection::setCardinality(ConnectionCardinality cardinality)
 {
 	this->cardinality = cardinality;
+	this->text = ConnectionCardinalityNames[cardinality];
 }
 
 ERD_Connection::~ERD_Connection(void)
